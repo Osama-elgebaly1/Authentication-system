96d11f61b008b7b4d5eb9de21e83b39d80b86d31
osama <osamaelgebaly122@gmail.com>
2024-11-01 07:44:25 +0200

    connecting the auth app with the main project

---
+++ Auth/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def regitster(request):
+    pass
+
+def Login(request):
+    pass
+
+def logout(request):
+    pass
+
+def profile(request):
+    pass
